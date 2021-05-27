96afdebdf688cb05b1eead7030ef2c3c87820d14
Ezgi Cevahir Bulut <31685120+ezgicevahirbulut@users.noreply.github.com>
2021-05-27 14:44:11 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Task } from './shared/task.model';
+import { TaskAddComponent } from './task-add/task-add.component';
+import { TaskEditComponent } from './task-edit/task-edit.component';
+import { UserAddComponent } from './user-add/user-add.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { UserManageComponent } from './user-manage/user-manage.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'users', component:UsersComponent},
+  {path:'users/add',component:UserAddComponent},
+  {path:'users/manage',component:UserManageComponent, children:[
+    {path:':id', component:UserEditComponent}
+  ]},
+  {path:'task',component:Task},
+  {path:'task/add',component:TaskAddComponent},
+  {path:'task/:id',component:TaskEditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
